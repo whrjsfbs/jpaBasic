6392226caf31e52dccf33c28f47db41bb66beae2
whrjsfbs <whrjsfbs@gmail.com>
2021-05-30 00:21:08 +0900

    객체지향스럽지 않은 상황 RDB에 맞추어서 Entity Mapping한것

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,36 +1,28 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+@Getter
+@Setter
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",
+        initialValue = 1, allocationSize = 50
+)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
-    private String name;
+
+    @Column(name = "name")
+    private String username;
 
     public Member() {
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,11 +1,12 @@
-package hellojpa;
+package jpabook.jpashop;
+
+import hellojpa.Member;
+import jpabook.jpashop.domain.Order;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.beans.PersistenceDelegate;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,14 +16,13 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("A");
-            Member member2 = new Member();
-            member2.setUsername("B");
+            Order order = em.find(Order.class, 1L);
+            Long memberId = order.getMemberId();
 
-            em.persist(member);
-            em.persist(member2);
-
+            //객체지향이 아닌듯한 느낌...
+            Member member = em.find(Member.class, memberId);
+            //이게 객체지향스럽지
+            //Member member = order.getMember();
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
